d0dcc4b35cc606e55e9b129c8e9150d3ef5461af
Robert J. Lambert III <rlambert85@gmail.com>
2019-02-17 09:46:55 -0800

    Fixed some basic errors in the transmission structs

--- transmission/transmission.go
+++ transmission/transmission.go
@@ -2,23 +2,27 @@ package transmission
 
 // Request is a generic request for transmission
 type Request struct {
-	Method    string    `json:"method`
+	Method    string    `json:"method"`
 	Arguments Arguments `json:"arguments"`
 }
 
+// Arguments are a list of required or optional fields for our requests
 type Arguments struct {
 	Fields Fields `json:"fields"`
-	IDs
+	IDs    IDs    `json:"ids"`
 }
 
+// IDs is a list of ids to filter out requests to
 type IDs struct {
 	IDs []int `json:"ids"`
 }
 
+// Fields is a list of fields to filter our requests to
 type Fields struct {
 	Fields []string `json:"fields"`
 }
 
+// TorrentsResponse is a response struct for torrent-get
 type TorrentsResponse struct {
 	Arguments []interface{} `json:"arguments"`
 	Result    string        `json:"result"`
